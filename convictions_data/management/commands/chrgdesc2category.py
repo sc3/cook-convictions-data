e3f1f06fe57c09345356b95390600b88610c225d
Brian Peterson <bepetersn@gmail.com>
2014-08-19 21:06:18 -0500

    add logging back in...

--- convictions_data/management/commands/chrgdesc2category.py
+++ convictions_data/management/commands/chrgdesc2category.py
@@ -10,7 +10,8 @@ from convictions_data.statute import (get_iucr, IUCRLookupError,
 from pprint import pprint
 import json
 
-logger = logging.getLogger(__name__)
+# can't find a handler?
+#logger = logging.getLogger(__name__)
 
 
 def append_or_create(dict, chrgdesc, category):
@@ -21,7 +22,9 @@ def append_or_create(dict, chrgdesc, category):
                 dict[chrgdesc].append(category)
         except KeyError:
             dict[chrgdesc] = [category]
-
+    else:
+        # warn if there's no IUCR category for this disposition
+        assert False
 
 class Command(BaseCommand):
     help = "Map charge descriptions to iucr categories."
@@ -35,7 +38,19 @@ class Command(BaseCommand):
             chrgdesc = disposition.ammndchrgdescr if \
                     disposition.ammndchrgdescr else disposition.chrgdesc
             category = disposition.iucr_category
-            append_or_create(chrgdesc_to_category, chrgdesc, category)
+
+            case_number = disposition.case_number
+            statute = disposition.final_statute if \
+                    disposition.final_statute else disposition.statute
+            chrgdisp = disposition.chrgdisp
+            chrgdispdate = disposition.chrgdispdate
+
+            try:
+                append_or_create(chrgdesc_to_category, chrgdesc, category)
+            except AssertionError:
+                # print('No IUCR category for disposition: {} {} {} {}'
+                #         .format(case_number, statute, chrgdispdate, chrgdisp))
+                pass
 
         print('num total: ', len(chrgdesc_to_category))
         with open('chrgdesc_to_category__all.json', 'w') as f:
